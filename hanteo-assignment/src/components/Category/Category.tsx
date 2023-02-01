7dc97a415b32bc8434aa0be695413b3e8ad88cb4
hxunz <guswl2843@gmail.com>
2023-02-01 13:45:17 +0900

    이벤트, 뉴스 ,스토어 카테고리 생성

--- hanteo-assignment/src/components/Category/Category.tsx
+++ hanteo-assignment/src/components/Category/Category.tsx
@@ -3,19 +3,28 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 function Category() {
-
   return (
-    <AppBar>
-      <Box sx={{ display: 'flex', backgroundColor: '#FFB0B2' }}>
-        <Button sx={{ my: 2, color: 'white' }} href='/chart'>
-          차트
-        </Button>
-        <Button sx={{ my: 2, color: 'white' }} href='/Whook'>
-          Whook
-        </Button>
-      </Box>
-
-    </AppBar>
+    <>
+      <AppBar>
+        <Box sx={{ display: 'flex', backgroundColor: '#FFB0B2' }}>
+          <Button sx={{ my: 2, color: 'white' }} href='/chart'>
+            차트
+          </Button>
+          <Button sx={{ my: 2, color: 'white' }} href='/Whook'>
+            Whook
+          </Button>
+          <Button sx={{ my: 2, color: 'white' }} href='/event'>
+            이벤트
+          </Button>
+          <Button sx={{ my: 2, color: 'white' }} href='/news'>
+            뉴스
+          </Button>
+          <Button sx={{ my: 2, color: 'white' }} href='/store'>
+            스토어
+          </Button>
+        </Box>
+      </AppBar>
+    </>
   );
 }
 export default Category;
