e1c3343fefc47355e25ec6275f238d8f65c34a7e
hxunz <guswl2843@gmail.com>
2023-02-03 01:51:43 +0900

    contents 영역에서 사용할 mock data 생성

---
+++ hanteo-assignment/src/mockContents.ts
@@ -0,0 +1,29 @@
+import { Content } from './redux/contentsSlice';
+
+export const mockContents: Content[] = [
+  {
+    id: 1,
+    url: 'https://photocloud.sbs.co.kr/origin/edit/S01_V0000010182/6368ad249b48857f98b8202d-p.jpg'
+  },
+  {
+    id: 2,
+    url: 'https://mblogthumb-phinf.pstatic.net/MjAyMTA1MjhfOTEg/MDAxNjIyMTk1OTU1MjAy.LT5gLpkf677PoEBq1mjuPwa2Gz-LkCCFGdw5u_ViC8Ug.Zj9AyHesAY8I9vpU3krBwqYyjZlzt6FrHpxiJytlIXkg.JPEG.ghkddbswjdzz/%EC%85%94%EB%88%842.jpg?type=w800'
+  },
+  {
+    id: 3,
+    url: 'https://pbs.twimg.com/media/FnuDsRqaQAA5srN?format=jpg&name=large'
+  },
+  {
+    id: 4,
+    url: 'https://pbs.twimg.com/media/Fio1yO-UcAAllP6?format=jpg&name=large'
+  },
+  {
+    id: 5,
+    url: 'https://pbs.twimg.com/media/FTLmRjNUAAEuLHn?format=jpg&name=large'
+  },
+  {
+    id: 6,
+    url: 'https://pbs.twimg.com/media/Fio1yO-UcAAllP6?format=jpg&name=large'
+  }
+];
+
