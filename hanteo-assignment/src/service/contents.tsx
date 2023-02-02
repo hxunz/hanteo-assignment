2c31448f8cf26451e76c54c3f4d15d03f7eca2a3
hxunz <guswl2843@gmail.com>
2023-02-03 01:53:40 +0900

    서버에서 데이터를 넘겨준다는 가정하에 mock 데이터 api 호출 기능 구현

--- hanteo-assignment/src/service/contents.tsx
+++ hanteo-assignment/src/service/contents.tsx
@@ -1,11 +1,11 @@
-import { httpClient } from './api';
+import { mockContents } from '../mockContents';
+import { Content } from '../redux/contentsSlice';
 
 export const getContents = async () => {
   try {
-    const { data } = await httpClient.get('/posts');
-
-    return data;
-
+    return new Promise<Content[]>((resolve) => setTimeout(() => {
+      resolve(mockContents);
+    }, 1500));
   } catch (error) {
     console.error(error);
   }
